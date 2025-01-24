6f3c943bb50963874b31e15846abf7488d9a9251
Kevin George Buhain <keving.buhain@gmail.com>
2025-01-24 15:56:35 -0500

    add weapon list and getCombat

--- lab03.py
+++ lab03.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+# Dice Options created using list() and range()
+diceOptions = list(range[1,7])
+
+# Weapons list
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+print("Available Weapons: ", ', '.join(weapons))
+
+def getCombatStrength(Prompt):
+    while True:
+        value = int(input(prompt))
+        if 1<= value <= 6:
+            return value
+        else:
+            print("Invalid Input: Please enter a number between 1-6")
